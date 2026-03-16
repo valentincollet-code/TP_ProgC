928be972da968c9c4f34dcfccc7ebca5ccefdfd2
valentincollet-code <valentin.collet@2028.icam.fr>
2026-03-16 21:07:44 +0000

    Exercice 1.5

---
+++ src/TP1/operateurs.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    // Initialisation des variables
+    int a = 16;
+    int b = 3;
+
+    printf("Valeurs : a = %d, b = %d\n\n", a, b);
+
+    // 1. Addition
+    printf("Addition (a + b) : %d\n", a + b);
+
+    // 2. Soustraction
+    printf("Soustraction (a - b) : %d\n", a - b);
+
+    // 3. Multiplication
+    printf("Multiplication (a * b) : %d\n", a * b);
+
+    // 4. Division (Attention : division entiere ici)
+    printf("Division (a / b) : %d\n", a / b);
+
+    // 5. Modulo (Le reste de la division)
+    printf("Modulo (a %% b) : %d\n", a % b);
+
+    // 6. Comparaison d'egalite (==)
+    // On affiche le resultat sous forme d'entier (0 ou 1)
+    printf("Est egal (a == b) : %d\n", a == b);
+
+    // 7. Comparaison de superiorite (>)
+    printf("Est superieur (a > b) : %d\n", a > b);
+
+    return 0;
+}
