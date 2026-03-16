02c2f3281e9a419baea2098bf7ac587e1a66af1f
valentincollet-code <valentin.collet@2028.icam.fr>
2026-03-16 21:19:25 +0000

    Exercice 1.6 - Versions for et while

---
+++ src/TP1/boucles_while.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int compteur = 5;
+    int i = 1;
+
+    while (i <= compteur) {
+        int j = 1;
+        while (j <= i) {
+            if (j == 1 || j == i || i == compteur) {
+                printf("* ");
+            } else {
+                printf("# ");
+            }
+            j++;
+        }
+        printf("\n");
+        i++;
+    }
+    return 0;
+}
