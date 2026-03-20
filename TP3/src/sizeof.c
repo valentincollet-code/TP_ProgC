fe36617256df1d042c1958d66ed2e49bc53eeefe
valentincollet-code <valentin.collet@2028.icam.fr>
2026-03-20 14:59:16 +0000

    Exercice 3.1

---
+++ TP3/src/sizeof.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    printf("La taille de int est : %zu octets\n", sizeof(int));
+    printf("La taille de int* est : %zu octets\n", sizeof(int*));
+    printf("La taille de int** est : %zu octets\n", sizeof(int**));
+    printf("La taille de char* est : %zu octets\n", sizeof(char*));
+    printf("La taille de float* est : %zu octets\n", sizeof(float*));
+    
+    return 0;
+}
+
+
+
+
+
+
+
+    
+
