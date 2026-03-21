960f4191de43b6cc2afd6e7e28e1c5d9b70ae086
valentincollet-code <valentin.collet@2028.icam.fr>
2026-03-21 11:17:09 +0100

    Exercice 1.6

---
+++ TP1/src/boucles.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int compteur = 8; // Cette valeur peut-être modifié
+
+    for (int i = 1; i <= compteur; i++) {
+        for (int j = 1; j <= i; j++) {
+            // Si on est sur les bords du triangle ou à la dernière ligne
+            if (j == 1 || j == i || i == compteur) {
+                printf("* ");
+            } else {
+                printf("# ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
