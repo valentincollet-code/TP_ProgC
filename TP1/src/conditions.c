b6c5652313538b999c1ab22bd2d28c7ee99d17db
valentincollet-code <valentin.collet@2028.icam.fr>
2026-03-21 11:22:03 +0100

    Exercice 1.7

--- TP1/src/conditions.c
+++ TP1/src/conditions.c
@@ -6,13 +6,13 @@ int main() {
     // 1. Boucle de 1 à 1000
     for (int i = 1; i <= 1000; i++) {
         
-        // 2. Condition de divisibilité par 11 (Prioritaire)
+        // 2. Condition: nombre divisible par 11 (Prioritaire)
         if (i % 11 == 0) {
             // On utilise continue pour passer directement au nombre suivant
             continue;
         }
 
-        // 3. Condition de divisibilité par 5 ou par 7
+        // 3. Condition: nombre divisible par 5 ou par 7
         if (i % 5 == 0 || i % 7 == 0) {
             somme += i; // Équivalent à somme = somme + i
         }
