80cae4878899094ae84e168a275164362833c136
valentincollet-code <valentin.collet@2028.icam.fr>
2026-04-03 12:22:48 +0200

    Exercice 3.1 terminé

--- TP3/src/sizeof.c
+++ TP3/src/sizeof.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 
 int main() {
+    // Affichage des tailles pour les entiers et leurs pointeurs
     printf("La taille de int est : %zu octets\n", sizeof(int));
     printf("La taille de int* est : %zu octets\n", sizeof(int*));
     printf("La taille de int** est : %zu octets\n", sizeof(int**));
+
+    // Affichage pour les pointeurs de caractères
     printf("La taille de char* est : %zu octets\n", sizeof(char*));
+    printf("La taille de char** est : %zu octets\n", sizeof(char**));
+    printf("La taille de char*** est : %zu octets\n", sizeof(char***));
+
+    // Affichage pour les pointeurs de flottants
     printf("La taille de float* est : %zu octets\n", sizeof(float*));
-    
+    printf("La taille de float** est : %zu octets\n", sizeof(float**));
+    printf("La taille de float*** est : %zu octets\n", sizeof(float***));
+
     return 0;
 }
-
-
 
 
 
